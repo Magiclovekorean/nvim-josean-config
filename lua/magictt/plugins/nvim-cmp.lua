3f7828f37c483fbccbf71aff9b5bb9f8785ec426
Martí Forn <magiclovett@gmail.com>
2026-02-19 22:58:03 +0100

    TmuxNavigateDown

--- lua/magictt/plugins/nvim-cmp.lua
+++ lua/magictt/plugins/nvim-cmp.lua
@@ -1,4 +1,3 @@
-
 return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
@@ -40,6 +39,7 @@ return {
         ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
@@ -57,9 +57,10 @@ return {
         format = lspkind.cmp_format({
           maxwidth = 50,
           ellipsis_char = "...",
+
         }),
       },
     })
   end,
-}
 
+}
