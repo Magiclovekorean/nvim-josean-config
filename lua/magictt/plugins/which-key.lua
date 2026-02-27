71662fc6355e18edf5512d0e464a2d17c91066c4
martí <magiclovett@gmail.com>
2026-02-27 07:24:42 +0100

    Update keymap group descriptions

--- lua/magictt/plugins/which-key.lua
+++ lua/magictt/plugins/which-key.lua
@@ -5,9 +5,21 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+  opts = {},
+  config = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>d", group = "Diagnostics" },
+      { "<leader>e", group = "Explorer" },
+      { "<leader>f", group = "Find" },
+      { "<leader>l", group = "Lazy panels" },
+      { "<leader>m", group = "Mason" },
+      { "<leader>n", group = "NoHighlight" },
+      { "<leader>p", group = "File Explorer" },
+      { "<leader>s", group = "Splits" },
+      { "<leader>t", group = "Tabs" },
+      { "<leader>w", group = "Workspace" },
+      { "<leader>x", group = "Trouble" },
+    })
+  end,
 }
