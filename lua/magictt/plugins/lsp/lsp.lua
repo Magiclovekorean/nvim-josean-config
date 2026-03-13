c8ad612640d8c54e15428f1e543d382498db2d60
martí <magiclovett@gmail.com>
2026-03-13 22:54:47 +0100

    Updated for betterpython autosuggestions

--- lua/magictt/plugins/lsp/lsp.lua
+++ lua/magictt/plugins/lsp/lsp.lua
@@ -1,28 +1,30 @@
 return {
-    {
-      "hrsh7th/cmp-nvim-lsp", event = { "BufReadPre", "BufNewFile" },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-       { "antosha417/nvim-lsp-file-operations", config = true },
-       { "folke/lazydev.nvim", opts = {} },
-       { "neovim/nvim-lspconfig" },
-       },
+      { "antosha417/nvim-lsp-file-operations", config = true },
+      { "folke/lazydev.nvim", opts = {} },
+      { "neovim/nvim-lspconfig" },
+    },
     config = function()
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-       -- import cmp-nvim-lsp plugin
-       local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      local capabilities = cmp_nvim_lsp.default_capabilities()
 
-       -- used to enable autocompletion (assign to every lsp server config)
-       local capabilities = cmp_nvim_lsp.default_capabilities()
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
 
+      vim.lsp.enable("pyright")
 
-       -- Auto-format on save for web files
-       vim.api.nvim_create_autocmd("BufWritePre", {
+      vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = { "*.js", "*.ts" },
         callback = function()
           vim.lsp.buf.format({ async = true })
         end,
-       })
-
+      })
     end,
   },
 }
+
